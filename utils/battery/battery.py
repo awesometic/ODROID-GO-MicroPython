aa54b6d4e74f54897828596f6903758fe10841df
Joshua Yang <joshua.yang@hardkernel.com>
2018-07-12 14:11:46 +0900

    initial commit

---
+++ utils/battery/battery.py
@@ -0,0 +1,15 @@
+"""
+A simple battery module for Micropython, only for ODROID-GO (ESP32).
+
+Created on:
+Author: Joshua Yang (joshua.yang@hardkernel.com)
+"""
+
+from machine import Pin
+
+
+class Battery:
+
+    def __init__(self):
+        pass
+
---
+++ utils/lcd/__init__.py
@@ -0,0 +1,3 @@
+from .ili934xnew import ILI9341
+from .colors import *
+from . import glcdfont, tt14, tt24, tt32
