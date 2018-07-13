543a4f3a38e07c4a0a6f7944fcef315c7c0cfde9
Joshua Yang <joshua.yang@hardkernel.com>
2018-07-13 13:40:58 +0900

    boot.py: Add simple description to call the welcome function

--- boot.py
+++ boot.py
@@ -4,9 +4,12 @@ An example boot.py for ODROID-GO module
 
 from odroid_go import GO
 
-print("ODROID-GO module loaded.")
+print("\033[1;32mGO: ODROID-GO module loaded\033[1;m")
+print("\033[1;32mGO: Enter \"hello()\" to see a message on the screen.\033[1;m")
 
 
 def hello():
     GO.lcd.fill(color=GO.lcd.colors.BLACK)
+    GO.lcd.set_font(GO.lcd.fonts.TT24)
+    GO.lcd.set_color(fg=GO.lcd.colors.GREEN, bg=GO.lcd.colors.BLACK)
     GO.lcd.print("Hello, ODROID-GO!")
