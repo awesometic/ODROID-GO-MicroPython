b39056ba9d75e4fa564adde2751461faa6f2638b
Joshua Yang <joshua.yang@hardkernel.com>
2018-07-16 16:11:04 +0900

    boot.py: Change the font size bigger

--- boot.py
+++ boot.py
@@ -10,6 +10,6 @@ print("\033[1;32mGO: Enter \"hello()\" to see a message on the screen.\033[1;m")
 
 def hello():
     GO.lcd.fill(color=GO.lcd.colors.BLACK)
-    GO.lcd.set_font(GO.lcd.fonts.TT24)
+    GO.lcd.set_font(GO.lcd.fonts.TT32)
     GO.lcd.set_color(fg=GO.lcd.colors.GREEN, bg=GO.lcd.colors.BLACK)
     GO.lcd.print("Hello, ODROID-GO!")
